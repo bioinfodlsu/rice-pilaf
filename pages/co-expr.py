ad1eee9b591b10131bd7f0d6231c2e3aa8881c22
anishmss <anish.ms.shrestha@gmail.com>
2023-03-18 16:49:04 +0800

    added dash_cytoscape

--- pages/co-expr.py
+++ pages/co-expr.py
@@ -2,21 +2,26 @@ import dash
 from dash import dcc, html
 
 import dash_cytoscape as cyto
+import networkx as nx
 
 dash.register_page(__name__, name="Co-expression Network Analysis")
+
+#G = nx.path_graph(3)
+coexpress_nw  = "data/networks/OS-CX.txt.1000" #path needs to be relative to top-level folder
+G = nx.read_edgelist(coexpress_nw, data=(("coexpress",float),))
+print("converting to cytoscape JSON")
+cyto_G = nx.cytoscape_data(G)
+
+#print(cyto_G['elements']['nodes'])
 
 layout = html.Div(
     [
         dcc.Markdown("Co-expression action happens here"),
         cyto.Cytoscape(
-        id='cytoscape-two-nodes',
-        layout={'name': 'preset'},
-        style={'width': '100%', 'height': '400px'},
-        elements=[
-            {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
-            {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
-            {'data': {'source': 'one', 'target': 'two'}}
-        ]
-    )
+            id='cytoscape-two-nodes',
+            layout={'name': 'cose'},
+            style={'width': '100%', 'height': '400px'},
+            elements = cyto_G['elements']
+        )
     ]
 )
