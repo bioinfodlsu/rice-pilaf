f4f163220b8852a1fa3fd628e61ef4c21bc47889
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-22 03:15:51 +0800

    Display tabs based on multiselect

--- callbacks/lift_over.py
+++ callbacks/lift_over.py
@@ -1,14 +1,94 @@
 from dash import dcc, html, Input, Output, State
+from dash.exceptions import PreventUpdate
+
+import gffutils
+import pandas as pd
+
+from collections import namedtuple
+
+Genomic_interval = namedtuple('Genomic_interval',['chrom','start','stop'])
+
+#convert 'Chr01:10000-25000' to a Genomic_Interval named tuple
+def to_genomic_interval(interval_str):
+    chrom,interval=interval_str.split(":")
+    start,stop = interval.split("-")
+    start = int(start)
+    stop = int(stop)
+    return Genomic_interval(chrom,start,stop)
+
+##getting genes from Nipponbare
+def get_genes_from_Nb(Nb_intervals):
+
+    dfs = []
+    for Nb_interval in Nb_intervals:
+        #load and search GFF_DB of Nipponbare
+        db = gffutils.FeatureDB('data/annotations/Nb/IRGSPMSU.gff.db',keep_order=True)
+        genes_in_interval = list(db.region(region=(Nb_interval.chrom,Nb_interval.start,Nb_interval.stop),
+                                           completely_within=False,featuretype='gene'))
+
+        #TODO should be a better way to do this?
+        df = pd.DataFrame({
+            'name': [gene.id for gene in genes_in_interval],
+            'chrom':[gene.chrom for gene in genes_in_interval],
+            'start':[gene.start for gene in genes_in_interval],
+            'end':[gene.end for gene in genes_in_interval]
+        })
+        dfs.append(df)
+    return pd.concat(dfs,ignore_index=True)
+
+##get intervals from other refs that align to (parts) of the input loci
+def get_genes_from_other_ref(ref,Nb_intervals):
+    db_align = gffutils.FeatureDB("data/alignments/{0}/{0}.1to1.gff.db".format("Nb_"+str(ref)))
+    db_annotation = gffutils.FeatureDB("data/annotations/{0}/{0}.gff.db".format(ref))
+    #get corresponding intervals on ref
+    dfs = []
+    for Nb_interval in Nb_intervals:
+        gff_intersections = list(db_align.region(region=(Nb_interval.chrom,Nb_interval.start,Nb_interval.stop),
+                                completely_within=False))
+        for intersection in gff_intersections:
+            ref_interval = to_genomic_interval(intersection.attributes['Name'][0])
+            genes_in_interval = list(db_annotation.region(region=(ref_interval.chrom,ref_interval.start,ref_interval.stop),
+                                                   completely_within=False,featuretype='gene'))
+            df = pd.DataFrame({
+                'name': [gene.id for gene in genes_in_interval],
+                'chrom':[gene.chrom for gene in genes_in_interval],
+                'start':[gene.start for gene in genes_in_interval],
+                'end':[gene.end for gene in genes_in_interval]
+            })
+            dfs.append(df)
+    return pd.concat(dfs,ignore_index=True)
+
 
 def init_callback(app):
     @app.callback(
-        Output('container-button-basic', 'children'),
-        Input('submit-val', 'n_clicks'),
-        State('input-on-submit', 'value')
+        Output('lift-over-results-intro', 'children'),
+        Output('lift-over-results-tabs', 'children'),
+        Input('lift-over-submit', 'n_clicks'),
+        Input('lift-over-other-refs', 'value')
     )
+
     def update_output(n_clicks, value):
-        return 'The input value was "{}" and the button has been clicked {} times'.format(
-            value,
-            n_clicks
-        )
+        if n_clicks >= 1:
+            tabs = ['NB'] + value
+            tabs_children = [dcc.Tab(label=tab) for tab in tabs]
+
+            # Nb_intervals = []
+            # for interval_str in Nb_intervals_str.split(";"):
+            #     Nb_intervals.append(to_genomic_interval(interval_str))
+
+            # df_Nb = get_genes_from_Nb(Nb_intervals)
+            # tabs[0].text("Genes overlapping the site in the Nipponbare reference")
+            # tabs[0].dataframe(df_Nb)
+
+
+            # for i,other_ref in enumerate(other_refs):
+            #     tabs[i+1].text("Genes from homologous regions in {}".format(other_ref))
+            #     tabs[i+1].dataframe(get_genes_from_other_ref(other_ref,Nb_intervals))
+
+
+            return 'The tabs below show a list of genes in Nipponbare and in homologous regions of the other references you chose', \
+                tabs_children
+                
+        
+        raise PreventUpdate
     
--- pages/lift-over.py
+++ pages/lift-over.py
@@ -1,64 +1,10 @@
 import dash
-from dash import dcc, html, Input, Output, State
-import gffutils
-import pandas as pd
-from collections import namedtuple
+from dash import dcc, html
 import dash_bootstrap_components as dbc
 
-Genomic_interval = namedtuple('Genomic_interval',['chrom','start','stop'])
 other_ref_genomes = ['N22','MH63']
 
 genomic_interval = 'Chr01:10000-20000;Chr01:22000-25000'
-
-#convert 'Chr01:10000-25000' to a Genomic_Interval named tuple
-def to_genomic_interval(interval_str):
-    chrom,interval=interval_str.split(":")
-    start,stop = interval.split("-")
-    start = int(start)
-    stop = int(stop)
-    return Genomic_interval(chrom,start,stop)
-
-##getting genes from Nipponbare
-def get_genes_from_Nb(Nb_intervals):
-
-    dfs = []
-    for Nb_interval in Nb_intervals:
-        #load and search GFF_DB of Nipponbare
-        db = gffutils.FeatureDB('data/annotations/Nb/IRGSPMSU.gff.db',keep_order=True)
-        genes_in_interval = list(db.region(region=(Nb_interval.chrom,Nb_interval.start,Nb_interval.stop),
-                                           completely_within=False,featuretype='gene'))
-
-        #TODO should be a better way to do this?
-        df = pd.DataFrame({
-            'name': [gene.id for gene in genes_in_interval],
-            'chrom':[gene.chrom for gene in genes_in_interval],
-            'start':[gene.start for gene in genes_in_interval],
-            'end':[gene.end for gene in genes_in_interval]
-        })
-        dfs.append(df)
-    return pd.concat(dfs,ignore_index=True)
-
-##get intervals from other refs that align to (parts) of the input loci
-def get_genes_from_other_ref(ref,Nb_intervals):
-    db_align = gffutils.FeatureDB("data/alignments/{0}/{0}.1to1.gff.db".format("Nb_"+str(ref)))
-    db_annotation = gffutils.FeatureDB("data/annotations/{0}/{0}.gff.db".format(ref))
-    #get corresponding intervals on ref
-    dfs = []
-    for Nb_interval in Nb_intervals:
-        gff_intersections = list(db_align.region(region=(Nb_interval.chrom,Nb_interval.start,Nb_interval.stop),
-                                completely_within=False))
-        for intersection in gff_intersections:
-            ref_interval = to_genomic_interval(intersection.attributes['Name'][0])
-            genes_in_interval = list(db_annotation.region(region=(ref_interval.chrom,ref_interval.start,ref_interval.stop),
-                                                   completely_within=False,featuretype='gene'))
-            df = pd.DataFrame({
-                'name': [gene.id for gene in genes_in_interval],
-                'chrom':[gene.chrom for gene in genes_in_interval],
-                'start':[gene.start for gene in genes_in_interval],
-                'end':[gene.end for gene in genes_in_interval]
-            })
-            dfs.append(df)
-    return pd.concat(dfs,ignore_index=True)
 
 dash.register_page(__name__, path ="/", name="Input and Lift-over")
 
@@ -74,13 +20,17 @@ layout = html.Div(
         html.Br(),
 
         dcc.Markdown('Search homologous regions of the following genomes:'),
-        dcc.Dropdown(other_ref_genomes, multi=True),
+        dcc.Dropdown(other_ref_genomes, id='lift-over-other-refs', multi=True),
 
         html.Br(),
 
-        html.Div(dcc.Input(id='input-on-submit', type='text')),
-        dbc.Button('Submit', id='submit-val', n_clicks=0),
-        html.Div(id='container-button-basic')
+        dbc.Button('Submit', id='lift-over-submit', n_clicks=0),
+
+        html.Br(),
+        html.Br(),
+
+        html.Div(id='lift-over-results-intro', children = ''),
+        dbc.Tabs(id="lift-over-results-tabs", children=[]),
     ]
 )
 
