94f709352c66d1c537a0da2d49378ead47b563f9
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-29 02:03:45 +0800

    Handle case where there are no homologous genes

--- callbacks/lift_over/util.py
+++ callbacks/lift_over/util.py
@@ -9,6 +9,7 @@ Genomic_interval = namedtuple('Genomic_interval',['chrom','start','stop'])
 NO_CHROM_INTERVAL_SEP = 1
 NO_START_STOP_SEP = 2
 START_STOP_NOT_INT = 3
+START_GREATER_THAN_STOP = 4
 
 # The integer returned refers to the error code
 def is_error(intervals):
@@ -31,6 +32,9 @@ def to_genomic_interval(interval_str):
         stop = int(stop)        
     except ValueError:
         return START_STOP_NOT_INT
+    
+    if start > stop:
+        return START_GREATER_THAN_STOP
 
     return Genomic_interval(chrom,start,stop)
 
@@ -55,6 +59,8 @@ def get_genomic_intervals_from_input(nb_intervals_str):
 def get_genes_from_Nb(Nb_intervals):
 
     dfs = []
+    if is_error(Nb_intervals):
+        Nb_intervals = []
 
     for Nb_interval in Nb_intervals:
         #load and search GFF_DB of Nipponbare
@@ -71,7 +77,11 @@ def get_genes_from_Nb(Nb_intervals):
         })
         dfs.append(df)
 
-    return pd.concat(dfs,ignore_index=True)
+    # Return empty dataframe if there are no results to concatenate
+    try:
+        return pd.concat(dfs,ignore_index=True)
+    except ValueError:
+        return pd.DataFrame()
 
 ##get intervals from other refs that align to (parts) of the input loci
 def get_genes_from_other_ref(ref,Nb_intervals):
@@ -79,6 +89,8 @@ def get_genes_from_other_ref(ref,Nb_intervals):
     db_annotation = gffutils.FeatureDB("data/annotations/{0}/{0}.gff.db".format(ref))
     #get corresponding intervals on ref
     dfs = []
+    if is_error(Nb_intervals):
+        Nb_intervals = []
         
     for Nb_interval in Nb_intervals:
         gff_intersections = list(db_align.region(region=(Nb_interval.chrom,Nb_interval.start,Nb_interval.stop),
@@ -95,4 +107,8 @@ def get_genes_from_other_ref(ref,Nb_intervals):
             })
             dfs.append(df)
 
-    return pd.concat(dfs,ignore_index=True)
+    # Return empty dataframe if there are no results to concatenate
+    try:
+        return pd.concat(dfs,ignore_index=True)
+    except ValueError:
+        return pd.DataFrame()
