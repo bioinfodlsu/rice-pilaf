be549b2068aafd9a2ecfcd69ca27654513263423
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-05 00:46:43 +0800

    Add script for converting MSU IDs in modules to KEGG transcript IDs

---
+++ enrichment_analysis/module-msu-to-transcript-id.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+
+
+def convert_msu_to_transcript(cluster_dir, mapping_file, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for cluster_file in os.listdir(cluster_dir):
+        with open(f'{cluster_dir}/{cluster_file}') as clusters, open(mapping_file, 'rb') as mapping, open(f'{output_dir}/{cluster_file[:-len(".txt")]}_transcript_id.txt', 'w') as output_file:
+            mapping_dict = pickle.load(mapping)
+
+            for node in clusters:
+                node = node.rstrip()
+                for transcript_id in mapping_dict[node]:
+                    output_file.write(f'{transcript_id}\n')
+
+
+if __name__ == '__main__':
+    convert_msu_to_transcript(
+        'data/clusters', 'data/msu-to-transcript-id.pickle', 'data/clusters_transcript_id')
