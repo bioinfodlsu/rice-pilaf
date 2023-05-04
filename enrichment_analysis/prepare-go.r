5007cc34ad9a730aeaf75ab847e3506eb8eb0b6f
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-05 01:03:00 +0800

    Remove header row

--- enrichment_analysis/prepare-go.r
+++ enrichment_analysis/prepare-go.r
@@ -3,4 +3,4 @@ library(GO.db)
 go_table <- as.data.frame(GOTERM)
 godb <- unique(go_table[,c(1,3,4)])
 
-write.table(goterms, sep="\t", quote=FALSE, file="data/go/go_terms.txt")
+write.table(goterms, sep="\t", quote=FALSE, file="data/go/go_terms.txt", col.names = FALSE)
