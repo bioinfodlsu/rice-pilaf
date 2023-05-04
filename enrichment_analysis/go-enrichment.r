41c23e65b14393d578691836a51843f7fd55eec7
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-05 01:25:29 +0800

    Perform GO enrichment analysis

--- enrichment_analysis/go-enrichment.r
+++ enrichment_analysis/go-enrichment.r
@@ -8,23 +8,29 @@ genes <- genes[[1]]
 background <- read.table("data/all_genes.txt")
 background <- background[[1]]
 
+gene_go <- read.table("data/go/agrigo.txt", stringsAsFactors = FALSE)
+gene_go <- gene_go[, 3:4]
+gene_go <- gene_go[, c(2,1)]
 
-kegg <- enrichKEGG(
-  gene = genes_transcript,
-  universe = background_transcript,
-  organism = "dosa",
-  keyType = "kegg",
+go_label <- read.table("data/go/go_terms.txt", sep = '\t', stringsAsFactors = FALSE)
+
+go <- enricher(gene = genes,
+               universe = background,
+               TERM2GENE = gene_go,  
+               TERM2NAME = go_label
 )
 
-kegg_df <- as.data.frame(kegg)
-write.table(kegg_df, "data/kegg_df.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+go_df <- as.data.frame(go)
+write.table(go_df, "data/output/go_df.txt", sep = "\t", row.names = FALSE, quote = FALSE)
 
-p1 <- dotplot(kegg,
-              showCategory = nrow(kegg_df),
-              title = "Top 10 most statistically significant enriched KEGG terms",
+p1 <- dotplot(go,
+              showCategory = nrow(go_df),
+              title = "Top 10 most statistically significant enriched GO terms",
               font.size = 10
 )
 ggsave(p1,
-       filename = "data/kegg_dotplot.png",
+       filename = "data/go_dotplot.png",
        height = 22, width = 22, units = "cm"
 )
+
+
