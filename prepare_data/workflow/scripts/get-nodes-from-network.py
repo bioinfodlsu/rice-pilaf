41b777f24c95daadf0197979221b58fe4293cd99
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-07 16:19:57 +0800

    Add script for getting nodes from network

---
+++ prepare_data/workflow/scripts/get-nodes-from-network.py
@@ -0,0 +1,33 @@
+import os
+
+
+def get_nodes(network_file, output_dir):
+    all_nodes = set()
+
+    with open(network_file) as network:
+        for edge in network:
+            edge = edge.rstrip()
+            nodes = edge.split('\t')
+
+            all_nodes.add(nodes[0])
+            all_nodes.add(nodes[1])
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(f'{output_dir}/all_genes.txt', 'w') as f:
+        f.write('\n'.join(list(all_nodes)))
+
+    print(f"Wrote {len(all_nodes)} nodes")
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'network_file', help='text file corresponding to the edge list')
+    parser.add_argument(
+        'output_dir', help='output directory for the list of nodes'
+    )
+
+    args = parser.parse_args()
