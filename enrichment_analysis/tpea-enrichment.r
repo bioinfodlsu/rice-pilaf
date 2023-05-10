f00aaf7c5c70161456509ab3160720e3b733d89d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-11 00:34:02 +0800

    Write script for mapping MSU to Entrez

--- enrichment_analysis/tpea-enrichment.r
+++ enrichment_analysis/tpea-enrichment.r
@@ -4,12 +4,12 @@ library(ggplot2)
 library(riceidconverter)
 library(clusterProfiler)
 
-DEGs<-sample(100:10000,10);
-DEG<-as.matrix(DEGs);
-number<-50;
-scores<-AUEC(DEG);
-FDR_method<-"fdr";
-results<-TPEA(DEG,scores,number,FDR_method);
+DEGs <- sample(100:10000, 10)
+DEG <- as.matrix(DEGs)
+number <- 50
+scores <- AUEC(DEG)
+FDR_method <- "fdr"
+results <- TPEA(DEG, scores, number, FDR_method)
 
 tpea_df <- as.data.frame(results)
 write.table(tpea_df, "data/tpea_df.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
@@ -19,7 +19,7 @@ write.table(tpea_df, "data/tpea_df.tsv", sep = "\t", row.names = FALSE, quote = 
 #               title = "Top 10 most statistically significant enriched KEGG terms",
 #               font.size = 10
 # )
-# 
+#
 # ggsave(p1,
 #        filename = "data/kegg_dotplot.png",
 #        height = 22, width = 22, units = "cm"
