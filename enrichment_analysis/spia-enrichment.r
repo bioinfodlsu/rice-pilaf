0c9ccfb35bb29a0e6d34620cdb8507aae19ddc36
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-11 16:56:12 +0800

    Distinguish between ORA and topology-based pathway enrichment analysis

--- enrichment_analysis/spia-enrichment.r
+++ enrichment_analysis/spia-enrichment.r
@@ -18,7 +18,7 @@ input_data <- setNames(dummy_fc, genes)
 
 background <- read.table("data/all_genes/transcript/all_genes.txt")
 background <- background[[1]]
-                     
+
 res <- spia(de = input_data, all = background, organism = "dosa", data.dir = "data/kegg_dosa/SPIA/")
 
 kegg_df <- as.data.frame(res)
