416886be5af2084079ab648ff7ae3407d9e0037b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-15 11:33:50 +0800

    Use circular layout for display

--- pages/co-expr.py
+++ pages/co-expr.py
@@ -6,21 +6,34 @@ from dash import dcc, html
 dash.register_page(__name__, name="Co-expression Network Analysis")
 
 # path needs to be relative to top-level folder
-coexpress_nw = "static/networks_display/clusterone/module-3.tsv"
+coexpress_nw = "static/networks_display/clusterone/module-25.tsv"
 G = nx.read_edgelist(coexpress_nw, data=(("coexpress", float),))
 cyto_G = nx.cytoscape_data(G)
-
-# Set the node label (user-facing)
-for node in cyto_G['elements']['nodes']:
-    node['data']['label'] = node['data']['name']
 
 layout = html.Div(
     [
         dcc.Markdown("Co-expression action happens here"),
         cyto.Cytoscape(
             id='cytoscape-two-nodes',
-            layout={'name': 'cose'},
-            style={'width': '100%', 'height': '400px'},
+            layout={'name': 'circle'},
+            style={'width': '100%', 'height': '200vh'},
+            stylesheet=[
+                {
+                    'selector': 'node',
+                    'style': {
+                        'content': 'data(id)',
+                        'height': '5px',
+                        'width': '5px',
+                        'font-size': '10px'
+                    }
+                },
+                {
+                    'selector': 'edge',
+                    'style': {
+                        'width': '1px',
+                    }
+                }
+            ],
             elements=cyto_G['elements']
         )
     ]
