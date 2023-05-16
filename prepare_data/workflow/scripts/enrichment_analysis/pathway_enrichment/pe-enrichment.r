fb562f917e5077cce611a80d9c46053c132f7d7b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-05-16 21:13:46 +0800

    Restructure project directory

---
+++ prepare_data/workflow/scripts/enrichment_analysis/pathway_enrichment/pe-enrichment.r
@@ -0,0 +1,68 @@
+library(data.table)
+library(ggplot2)
+library(graphite)
+library(tidyverse)
+library(ROntoTools)
+library(optparse)
+
+option_list <- list(
+    make_option(c("-g", "--modules"),
+        type = "character", default = NULL,
+        help = "text file containing the modules (gene IDs should be KEGG transcript IDs)"
+    ),
+    make_option(c("-i", "--module_index"),
+        type = "integer", default = NULL,
+        help = "index of the module of interest (first module is index 1)"
+    ),
+    make_option(c("-b", "--background_genes"),
+        type = "character", default = NULL,
+        help = "text file containing the background genes"
+    ),
+    make_option(c("-o", "--output_dir"),
+        type = "character", default = NULL,
+        help = "output directory for the data frame and plot showing the enriched pathways"
+    )
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+modules <- readLines(opt$modules)
+modules <- str_split(modules, "\t")
+
+genes <- paste0("dosa:", unlist(modules[opt$module_index]))
+dummy_val <- 20
+dummy_fc <- replicate(length(genes), dummy_val)
+input_data <- setNames(dummy_fc, genes)
+
+background <- readLines(opt$background_genes)
+background <- str_split(background, "\t")
+background <- paste0("dosa:", unlist(background))
+
+kpg <- keggPathwayGraphs("dosa")
+kpg <- setEdgeWeights(kpg)
+kpg <- setNodeWeights(kpg)
+
+pe_results <- pe(input_data,
+    graphs = kpg,
+    ref = background, nboot = 2000, verbose = TRUE
+)
+
+if (!dir.exists(opt$output_dir)) {
+    dir.create(opt$output_dir, recursive = TRUE)
+}
+
+if (!dir.exists(paste0(opt$output_dir, "/results"))) {
+    dir.create(paste0(opt$output_dir, "/results"), recursive = TRUE)
+}
+
+kegg_df <- head(summary(pe_results))
+write.table(kegg_df, paste0(opt$output_dir, "/results/pe-df-", opt$module_index, ".tsv"),
+    sep = "\t", row.names = TRUE, quote = FALSE
+)
+
+cat("\n")
+print(paste0(
+    "Generated data frame showing the enriched KEGG pathways for module #",
+    opt$module_index
+))
---
+++ prepare_data/workflow/scripts/enrichment_analysis/util/ricegeneid-msu-to-transcript-id.r
@@ -0,0 +1,50 @@
+library(data.table)
+library(riceidconverter)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-g", "--msu_genes"),
+    type = "character", default = NULL,
+    help = "text file containing the input genes (MSU ID)"
+  ),
+  make_option(c("-o", "--output_dir"),
+    type = "character", default = NULL,
+    help = "output directory for the text file containing the equivalent KEGG transcript IDs and the text file containing the genes without equivalent KEGG transcript IDs"
+  )
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+all_genes <- read.table(opt$msu_genes)
+all_genes <- all_genes[[1]]
+
+all_transcript <- c()
+na_all_transcript <- c()
+
+for (gene in all_genes) {
+  transcript_id <- RiceIDConvert(gene, fromType = "MSU", toType = "TRANSCRIPTID")
+  transcript_id_list <- c()
+  transcript_id_list <- append(transcript_id_list, gene)
+
+  for (id in transcript_id$TRANSCRIPTID) {
+    if (is.na(id)) {
+      print(gene)
+      na_all_transcript <- append(na_all_transcript, gene)
+    } else {
+      transcript_id_list <- append(transcript_id_list, id)
+    }
+  }
+
+  all_transcript <- append(all_transcript, list(transcript_id_list))
+}
+
+if (!dir.exists(opt$output_dir)) {
+  dir.create(opt$output_dir, recursive = TRUE)
+}
+
+lapply(na_all_transcript, write, paste0(opt$output_dir, "/all-na-transcript-id.txt"), append = TRUE, sep = "\n")
+print(paste0("Generated ", opt$output_dir, "/all-na-transcript-id.txt"))
+
+lapply(all_transcript, write, paste0(opt$output_dir, "/all-transcript-id.txt"), append = TRUE, sep = "\n")
+print(paste0("Generated ", opt$output_dir, "/all-transcript-id.txt"))
