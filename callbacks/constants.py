01072add840dc8b24e5d39bf023c7f06c069fd1a
unknown <phoebs_ong@dlsu.edu.ph>
2023-05-16 21:12:25 +0800

    Revise file paths

--- callbacks/constants.py
+++ callbacks/constants.py
@@ -1,25 +1,27 @@
 class Constants(object):
     DATA = 'static'
+    APP_DATA = f'{DATA}/app_data'
+    RAW_DATA = f'{DATA}/raw_data'
 
-    ANNOTATIONS = f'{DATA}/annotations'
-    ALIGNMENTS = f'{DATA}/alignments'
-    OGI_MAPPING = f'{DATA}/ogi_mapping'
+    ANNOTATIONS = f'{APP_DATA}/annotations'
+    ALIGNMENTS = f'{APP_DATA}/alignments'
+    OGI_MAPPING = f'{APP_DATA}/ogi_mapping'
 
-    IGV = f'{DATA}/igv'
-    GENOMES_NIPPONBARE = f'{DATA}/genomes/Nipponbare'
+    GENOMES_NIPPONBARE = f'{APP_DATA}/genomes/Nipponbare'
     ANNOTATIONS_NB = f'{ANNOTATIONS}/Nb'
-    OPEN_CHROMATIN = f'{DATA}/open_chromatin'
+    OPEN_CHROMATIN = f'{APP_DATA}/open_chromatin'
     OPEN_CHROMATIN_PANICLE = f'{OPEN_CHROMATIN}/panicle'
 
-    NETWORKS = f'{DATA}/networks'
-    NETWORKS_DISPLAY = f'{DATA}/networks_display'
+    NETWORKS = f'{RAW_DATA}/networks'
+    NETWORKS_DISPLAY = f'{APP_DATA}/networks_display'
     NETWORKS_DISPLAY_CLUSTERONE = f'{NETWORKS_DISPLAY}/clusterone'
     NETWORKS_DISPLAY_CLUSTERONE_MODULES = f'{NETWORKS_DISPLAY_CLUSTERONE}/modules'
 
     TEMP = f'{DATA}/temp'
     IMPLICATED_GENES = f'{TEMP}/implicated_genes'
+    TEMP_IGV = f'{TEMP}/igv'
 
-    ENRICHMENT_ANALAYSIS = 'enrichment_analysis'
+    ENRICHMENT_ANALAYSIS = 'prepare_data/workflow/scripts/enrichment_analysis'
     ENRICHMENT_ANALYSIS_OUTPUT = f'{ENRICHMENT_ANALAYSIS}/data/output'
     ENRICHMENT_ANALYSIS_OUTPUT_ONTOLOGY = f'{ENRICHMENT_ANALYSIS_OUTPUT}/ontology_enrichment'
     ENRICHMENT_ANALYSIS_OUTPUT_PATHWAY = f'{ENRICHMENT_ANALYSIS_OUTPUT}/pathway_enrichment'
