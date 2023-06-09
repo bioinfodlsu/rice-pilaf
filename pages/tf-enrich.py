69c271670ca65b52380beddd74278ede88eb2815
unknown <phoebs_ong@dlsu.edu.ph>
2023-06-09 20:05:18 +0800

    Change tfbs dropdown to radio buttons and add a submit button

--- pages/tf-enrich.py
+++ pages/tf-enrich.py
@@ -1,4 +1,5 @@
 import dash
+import dash_bootstrap_components as dbc
 from dash import dash_table, dcc, html
 
 dash.register_page(__name__, name="Regulatory feature enrichment")
@@ -10,24 +11,35 @@ layout = html.Div(
 
         html.Br(),
         dcc.Markdown("Consider TF binding sites in the following regions:"),
-        dcc.Dropdown(["genome-wide","promoters"],
-                     'promoters',
-                     id='tfbs_set',
-                     placeholder='Select a TFBS set',
-                     multi=False,
-                     persistence=True,
-                     persistence_type='memory'
+        dbc.RadioItems(
+            id='tfbs_set',
+            options=[
+                {'value': 'genome-wide', 'label': 'genome-wide',
+                 'label_id': 'genome-wide'},
+                {'value': 'promoters', 'label': 'promoters', 'label_id': 'promoters'}
+            ],
+            value='genome-wide',
+            inline=True
         ),
-       html.Br(),
-       dcc.Markdown("Choose TF binding site prediction technique:"),
-       dcc.Dropdown(["motif scan","motif conservation","FunTFBS"],
-                    "FunTFBS",
-                     id='tfbs_prediction_technique',
-                     placeholder='Select a TF binding site prediction technique',
-                     multi=False,
-                     persistence=True,
-                     persistence_type='memory'
+        html.Br(),
+        dcc.Markdown("Choose TF binding site prediction technique:"),
+        dbc.RadioItems(
+            id='tfbs_prediction_technique',
+            options=[
+                {'value': 'FunTFBS', 'label': 'FunTFBS', 'label_id': 'FunTFBS'},
+                {'value': 'motif conservation', 'label': 'motif conservation',
+                    'label_id': 'motif conservation'},
+                {'value': 'motif scan', 'label': 'motif scan',
+                 'label_id': 'motif scan'}
+            ],
+            value='FunTFBS',
+            inline=True
         ),
+        html.Br(),
+        dbc.Button('Submit',
+                   id='tfbs-submit',
+                   n_clicks=0,
+                   className='page-button'),
 
         dash_table.DataTable(
             id='tf_enrichment_result_table',
