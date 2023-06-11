524b3c4f31aaff9a519b2e0a0747de779fabecca
memgonzales <gonzales.markedward@gmail.com>
2023-06-12 01:55:53 +0800

    Remove tooltip and change div to label

--- pages/co-expr.py
+++ pages/co-expr.py
@@ -3,14 +3,13 @@ import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 from dash import dash_table, dcc, html
 
-dash.register_page(__name__, name="Co-expression Network Analysis")
+dash.register_page(__name__, name='Co-expression Network Analysis')
 
 
 layout = dbc.Row(dbc.Col(id='coexpression-container', children=[
-    html.Div(["Module detection algorithm ", html.I(
-        className="bi bi-info-circle-fill me-2", id="coexpression-clustering-algo-tooltip")]),
-
-    html.Br(),
+    dbc.Label(['Select a module detection algorithm ', html.I(
+        className='bi bi-info-circle mx-2', id='coexpression-clustering-algo-tooltip')],
+        className='mb-2'),
 
     dbc.RadioItems(
         id='coexpression-clustering-algo',
@@ -25,18 +24,13 @@ layout = dbc.Row(dbc.Col(id='coexpression-container', children=[
         inline=True
     ),
 
-    dbc.Tooltip('The algorithms below allow for overlapping modules (that is, genes may belong to more than one module). Hover over each algorithm for more details',
-                target='coexpression-clustering-algo-tooltip'),
-
-    dbc.Tooltip('Detects highly connected gene subnetworks and expands them by including closely associated genes. Reference: Wu, M., Li, X., Kwoh, C. K., & Ng, S. K. (2009). A core-attachment based method to detect protein complexes in PPI networks. BMC Bioinformatics, 10(169)',
-                target='coach'),
-
     html.Br(),
 
-    html.Div(["Parameter ", html.I(
-        className="bi bi-info-circle-fill me-2", id="coexpression-parameter-tooltip")]),
-
-    html.Br(),
+    dbc.Label(['Select the ',
+               html.Span('parameter', id='coexpression-parameter-name'),
+               html.I(
+                   className='bi bi-info-circle mx-2', id='coexpression-parameter-tooltip')],
+              className='mb-4'),
 
     dcc.Slider(id='coexpression-parameter-slider', step=None,
                marks={0: '0.0', 10: '0.1', 20: '0.2', 30: '0.3', 40: '0.4',
@@ -53,97 +47,99 @@ layout = dbc.Row(dbc.Col(id='coexpression-container', children=[
     html.Br(),
     html.Br(),
 
-    html.Div(id="coexpression-results-container", style={'display': 'none'}, children=[
-        dcc.Markdown("Enriched modules"),
+    html.Div(id='coexpression-results-container', style={'display': 'none'}, children=[
+        dbc.Label('Select an enriched module'),
 
-        dcc.Loading(dcc.Dropdown(
-            id='coexpression-modules',
-            style={'display': 'none'}
-        )),
+        dcc.Loading([
+            dcc.Dropdown(
+                id='coexpression-modules',
+                style={'display': 'none'}
+            ),
 
-        html.Br(),
+            html.Br(),
 
-        dbc.Tabs(id='coexpression-modules-pathway', active_tab='tab-0',
-                 children=[dcc.Tab(label='Gene Ontology',
-                                   value='Gene Ontology'),
-                           dcc.Tab(label='Trait Ontology',
-                                   value='Trait Ontology'),
-                           dcc.Tab(label='Plant Ontology',
-                                   value='Plant Ontology'),
-                           dcc.Tab(label='Pathways (Overrepresentation)',
-                                   value='Pathways (Overrepresentation)'),
-                           dcc.Tab(label='Pathway-Express',
-                                   value='Pathway-Express'),
-                           dcc.Tab(label='SPIA', value='SPIA')]),
+            dbc.Tabs(id='coexpression-modules-pathway', active_tab='tab-0',
+                     children=[dcc.Tab(label='Gene Ontology',
+                                       value='Gene Ontology'),
+                               dcc.Tab(label='Trait Ontology',
+                                       value='Trait Ontology'),
+                               dcc.Tab(label='Plant Ontology',
+                                       value='Plant Ontology'),
+                               dcc.Tab(label='Pathways (Overrepresentation)',
+                                       value='Pathways (Overrepresentation)'),
+                               dcc.Tab(label='Pathway-Express',
+                                       value='Pathway-Express'),
+                               dcc.Tab(label='SPIA', value='SPIA')]),
 
-        html.Br(),
+            html.Br(),
 
-        dash_table.DataTable(
-            id='coexpression-pathways',
-            persistence=True,
-            persistence_type='memory',
-            export_format='csv',
-            style_cell={
-                'whiteSpace': 'pre-line',
-                'font-family': 'sans-serif'
-            },
-            markdown_options={"html": True},
-            sort_action='native',
-            filter_action='native',
-            filter_options={'case': 'insensitive',
-                            'placeholder_text': 'Search column'},
-            page_action='native',
-            page_size=15
-        ),
-
-        html.Br(),
-
-        dcc.Markdown("Module"),
-
-        dbc.RadioItems(
-            id='coexpression-graph-layout',
-            options=[
-                {'value': 'circle', 'label': 'Circle', 'label_id': 'circle'},
-                {'value': 'grid', 'label': 'Grid', 'label_id': 'grid'}
-            ],
-            value='circle',
-            inline=True
-        ),
-
-        html.Br(),
-
-        cyto.Cytoscape(
-            id='coexpression-module-graph',
-            layout={'name': 'circle'},
-            style={'width': '100%',
-                   'height': '100vh'},          # Should be here (otherwise, initial loading does not consume entire width and height)
-            stylesheet=[
-                {
-                    'selector': 'node',
-                    'style': {
-                        'content': 'data(id)',
-                        'height': '5px',
-                        'width': '5px',
-                        'font-size': '10px'
-                    }
+            dash_table.DataTable(
+                id='coexpression-pathways',
+                persistence=True,
+                persistence_type='memory',
+                export_format='csv',
+                style_cell={
+                    'whiteSpace': 'pre-line',
+                    'font-family': 'sans-serif'
                 },
-                {
-                    'selector': 'edge',
-                    'style': {
-                        'width': '1px',
+                markdown_options={'html': True},
+                sort_action='native',
+                filter_action='native',
+                filter_options={'case': 'insensitive',
+                                'placeholder_text': 'Search column'},
+                page_action='native',
+                page_size=15
+            ),
+
+            html.Br(),
+
+            dcc.Markdown('Module'),
+
+            dbc.RadioItems(
+                id='coexpression-graph-layout',
+                options=[
+                    {'value': 'circle', 'label': 'Circle', 'label_id': 'circle'},
+                    {'value': 'grid', 'label': 'Grid', 'label_id': 'grid'}
+                ],
+                value='circle',
+                inline=True
+            ),
+
+            html.Br(),
+
+            cyto.Cytoscape(
+                id='coexpression-module-graph',
+                layout={'name': 'circle'},
+                style={'width': '100%',
+                       'height': '100vh'},          # Should be here (otherwise, initial loading does not consume entire width and height)
+                stylesheet=[
+                    {
+                        'selector': 'node',
+                        'style': {
+                            'content': 'data(id)',
+                            'height': '5px',
+                            'width': '5px',
+                            'font-size': '10px'
+                        }
+                    },
+                    {
+                        'selector': 'edge',
+                        'style': {
+                            'width': '1px',
+                        }
+                    },
+                    {
+                        'selector': '.shaded',
+                        'style': {
+                            'background-color': '#254b5d',
+                            'line-color': '#254b5d',
+                            'height': '20px',
+                            'width': '20px'
+                        }
                     }
-                },
-                {
-                    'selector': '.shaded',
-                    'style': {
-                        'background-color': '#254b5d',
-                        'line-color': '#254b5d',
-                        'height': '20px',
-                        'width': '20px'
-                    }
-                }
-            ]
-        )
+                ]
+            )
+        ])
     ])
 ]
 ))
