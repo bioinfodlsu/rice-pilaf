35366fddc4774f04ef7f75831148934288648570
unknown <phoebs_ong@dlsu.edu.ph>
2023-06-16 20:50:01 +0800

    Uncomment hide graph table

--- callbacks/coexpression/callbacks.py
+++ callbacks/coexpression/callbacks.py
@@ -66,7 +66,6 @@ def init_callback(app):
 
         return get_parameters_for_algo(algo), ALGOS_DEFAULT_PARAM[algo] * ALGOS_MULT[algo]
 
-    """
     @app.callback(
         Output('coexpression-module-graph', 'elements'),
         Output('coexpression-module-graph', 'layout'),
@@ -89,7 +88,7 @@ def init_callback(app):
                 implicated_gene_ids, None, submitted_algo, parameters, layout) + ({'visibility': 'hidden'}, )
 
         raise PreventUpdate
-    """
+
     @app.callback(
         Output('coexpression-modules', 'style'),
         Output('coexpression-modules', 'options'),
