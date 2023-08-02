e8650b9edbcdd20affd6a49861b3d06b0823d3b7
unknown <phoebs_ong@dlsu.edu.ph>
2023-08-02 20:52:53 +0800

    Changed dictionary to OrderedDict() in the get_analysis_layout_dictionary function

--- pages/analysis_layout.py
+++ pages/analysis_layout.py
@@ -5,16 +5,18 @@ import pages.analysis.co_expr as co_expr
 import pages.analysis.tf_enrich as tf_enrich
 import pages.analysis.browse_loci as browse_loci
 
+from collections import OrderedDict
+
 from callbacks.constants import Constants
 const = Constants()
 
 def get_analaysis_layout_dictionary():
-    return {
+    return OrderedDict({
         const.LIFT_OVER: 'Gene List and Lift-Over',
         const.COEXPRESSION: 'Co-Expression Network Analysis',
         const.TFBS: 'Regulatory Feature Enrichment',
         const.IGV: 'Browse Loci'
-    }
+    })
 
 layout = html.Div(children=[
     lift_over.layout, 
