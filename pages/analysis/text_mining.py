adc055c678bd4ad34f59fec085c2f1b171371e04
memgonzales <gonzales.markedward@gmail.com>
2023-08-13 16:01:52 +0800

    Make text mining table filterable and sortable

--- pages/analysis/text_mining.py
+++ pages/analysis/text_mining.py
@@ -7,13 +7,13 @@ layout = html.Div(
     id={
         'type': 'analysis-layout',
         'label': const.TEXT_MINING
-    }, 
+    },
     hidden=True,
     children=[
         html.Div([
             html.Span('In this page, you can retrieve gene names associated with traits, diseases, chemicals, etc. from a database constructed from text-mined PubMed abstracts. '
-                    #'Conversely, you can retrieve literature that associates your gene of interest to some phenotype.'
-            ),
+                      # 'Conversely, you can retrieve literature that associates your gene of interest to some phenotype.'
+                      ),
         ], className='analysis-intro p-3'),
 
         html.Div([
@@ -32,24 +32,24 @@ layout = html.Div(
             ),
 
             html.Div([html.Span('Examples:', className='pe-3'),
-                    html.Span('pre-harvest sprouting',
+                      html.Span('pre-harvest sprouting',
                                 id={'type': 'example-text-mining',
                                     'description': 'pre-harvest sprouting'},
                                 className='sample-genomic-interval'),
-                    html.Span(',', className='sample-genomic-interval'),
-                    html.Span('anaerobic germination',
+                      html.Span(',', className='sample-genomic-interval'),
+                      html.Span('anaerobic germination',
                                 id={'type': 'example-text-mining',
                                     'description': 'anaerobic germination'},
                                 className='sample-genomic-interval ms-3'),
-                    html.Span(',', className='sample-genomic-interval')
-                    ],
-                    className='pt-3'),
+                      html.Span(',', className='sample-genomic-interval')
+                      ],
+                     className='pt-3'),
             html.Br(),
 
             dbc.Button('Search',
-                    id='text-mining-submit',
-                    className='page-button',
-                    n_clicks=0),
+                       id='text-mining-submit',
+                       className='page-button',
+                       n_clicks=0),
 
             html.Br(),
             html.Br(),
@@ -67,19 +67,25 @@ layout = html.Div(
                                     id='text-mining-export-table',
                                     n_clicks=0,
                                     color='light', size='sm', className='table-button'),
-                                dcc.Download(id='text-mining-download-df-to-csv')
+                                dcc.Download(
+                                    id='text-mining-download-df-to-csv')
                             ], style={'textAlign': 'right'})
                         ),
 
                         dash_table.DataTable(
                             id='text_mining_result_table',
-                            persistence=True,
-                            persistence_type='memory',
                             style_data={
                                 'whiteSpace': 'normal',
                                 'height': 'auto',
-                                'textAlign': 'left'
-                            }
+                                'textAlign': 'left',
+                                'font-family': 'sans-serif'
+                            },
+                            sort_action='native',
+                            filter_action='native',
+                            filter_options={'case': 'insensitive',
+                                            'placeholder_text': 'Search column'},
+                            page_action='native',
+                            page_size=10
                         )
                     ])
                 ], className='p-3 mt-2')
