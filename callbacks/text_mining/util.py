9d10ca129fbe97030fff95569f14bbe10bf082e7
memgonzales <gonzales.markedward@gmail.com>
2023-08-13 15:24:21 +0800

    Specify encoding for reading annotated abstracts

--- callbacks/text_mining/util.py
+++ callbacks/text_mining/util.py
@@ -2,21 +2,23 @@ import pandas as pd
 from ..constants import Constants
 import re
 
-COLNAMES = ['Gene','PMID','Title','Sentence','Score']
+COLNAMES = ['Gene', 'PMID', 'Title', 'Sentence', 'Score']
+
 
 def text_mining_query_search(query_string):
     df = pd.DataFrame(columns=COLNAMES)
-    query_regex = re.compile(query_string,re.IGNORECASE)
-    with open(Constants.TEXT_MINING_ANNOTATED_ABSTRACTS,'r') as f:
+    query_regex = re.compile(query_string, re.IGNORECASE)
+    with open(Constants.TEXT_MINING_ANNOTATED_ABSTRACTS, 'r', encoding='utf8') as f:
         for line in f:
-            if re.search(query_regex,line):
+            if re.search(query_regex, line):
                 PMID, Title, Sentence, IsInTitle, Entity, Annotations, Type, start_pos, end_pos, score = line.split(
                     "\t")
 
                 if Type == 'Gene':
                     if Sentence == 'None':
                         Sentence = Title
-                    df.loc[len(df.index)] = [Entity,PMID,Title,Sentence,score]
+                    df.loc[len(df.index)] = [
+                        Entity, PMID, Title, Sentence, score]
 
     if len(df.index) > 0:
         return df
