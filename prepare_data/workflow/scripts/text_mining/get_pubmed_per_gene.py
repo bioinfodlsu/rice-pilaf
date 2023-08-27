b4c4461caed2c1b8e7b3672833a6d99ae0aaed06
memgonzales <gonzales.markedward@gmail.com>
2023-08-27 22:00:06 +0800

    Handle symbols that have to be replaced

--- prepare_data/workflow/scripts/text_mining/get_pubmed_per_gene.py
+++ prepare_data/workflow/scripts/text_mining/get_pubmed_per_gene.py
@@ -59,58 +59,17 @@ def get_pubmed_per_gene(accession, gene_symbols, annotated_abstracts, output_dir
     query_str_with_case = ''
 
     for symbol in gene_symbols:
-        if len(symbol) != 2:
-            query_str_ignore_case += f'({re.escape(symbol)})|'
+        if symbol in symbols_to_be_replaced:
+            for replacement_symbol in symbols_to_be_replaced[symbol]:
+                query_str_with_case += f'({re.escape(replacement_symbol)})|'
         else:
-            query_str_with_case += f'({re.escape(symbol)})|'
-
-    query_str_ignore_case = query_str_ignore_case[:-1]
-    query_str_with_case = query_str_with_case[:-1]
-
-    pmid_score_ignore_case = None
-    pmid_score_with_case = None
-
-    if query_str_ignore_case:
-        # Should not be sandwiched between alphanumeric characters
-        query_str_ignore_case = f'(?<![a-zA-Z0-9])({query_str_ignore_case})(?![a-zA-Z0-9])'
-        pmid_score_ignore_case = perform_single_query(
-            query_str_ignore_case, annotated_abstracts, ignore_case=True)
-    if query_str_with_case:
-        # Should not be sandwiched between alphanumeric characters
-        query_str_with_case = f'(?<![a-zA-Z0-9])({query_str_with_case})(?![a-zA-Z0-9])'
-        pmid_score_with_case = perform_single_query(
-            query_str_with_case, annotated_abstracts, ignore_case=False)
-
-    pmid_score = {}
-    if pmid_score_ignore_case:
-        for pmid, score in pmid_score_ignore_case.items():
-            pmid_score[pmid] = score
-
-    if pmid_score_with_case:
-        for pmid, score in pmid_score_with_case.items():
-            if pmid in pmid_score:
-                pmid_score[pmid] = max(pmid_score[pmid], score)
-            else:
-                pmid_score[pmid] = score
-
-    if pmid_score:
-        with open(f'{output_directory}/{accession}.pickle', 'wb') as f:
-            pickle.dump(pmid_score, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def get_pubmed_per_gene_english_sensitive(accession, gene_symbols, annotated_abstracts, output_directory):
-    query_str_ignore_case = ''
-    query_str_with_case = ''
-
-    for symbol in gene_symbols:
-        # English words should be case-sensitive (so that 'map' will be disambiguated from 'MAP')
-        if symbol.lower() in ENG_WORDS:
-            query_str_with_case += f'({re.escape(symbol)})|'
-        else:
-            if len(symbol) != 2:
-                query_str_ignore_case += f'({re.escape(symbol)})|'
-            else:
+            if symbol.lower() in ENG_WORDS:
                 query_str_with_case += f'({re.escape(symbol)})|'
+            else:
+                if len(symbol) != 2:
+                    query_str_ignore_case += f'({re.escape(symbol)})|'
+                else:
+                    query_str_with_case += f'({re.escape(symbol)})|'
 
     query_str_ignore_case = query_str_ignore_case[:-1]
     query_str_with_case = query_str_with_case[:-1]
@@ -230,12 +189,57 @@ def handle_english_symbols(gene_index, annotated_abstracts, output_directory):
                 for accession in accessions:
                     accession = accession.strip()
                     if accession:
-                        get_pubmed_per_gene_english_sensitive(accession, gene_symbols + [accession] + iricname + raprepname + rappredname,
-                                                              annotated_abstracts, output_directory)
+                        get_pubmed_per_gene(accession, gene_symbols + [accession] + iricname + raprepname + rappredname,
+                                            annotated_abstracts, output_directory)
 
                         print(f'Finished parsing entry for {accession}')
 
     print(f'Finished post-processing handling English gene symbols')
+
+
+def handle_symbol_replacement(gene_index, annotated_abstracts, output_directory):
+    with open(gene_index, encoding='utf8') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        next(csv_reader)
+
+        for line in csv_reader:
+            iricname = list(filter(None, line[1].strip().split(',')))
+            raprepname = list(filter(None, line[3].strip().split(',')))
+            rappredname = list(filter(None, line[4].strip().split(',')))
+
+            # Some entries in the accession column consist of multiple accessions
+            accessions = line[2].split(',')
+            # Remove the opening and closing brackets
+            gene_symbols = line[-1][1:-1].split(',')
+            gene_symbols = [gene_symbol.replace('"', '').replace(
+                "'", '').replace('\\', '').strip() for gene_symbol in gene_symbols]
+
+            is_there_symbol_to_be_replaced = False
+            for idx, gene_symbol in enumerate(gene_symbols):
+                if gene_symbol in symbols_to_be_replaced:
+                    is_there_symbol_to_be_replaced = True
+
+                if gene_symbol.isdigit():
+                    # Handle cases like \\OsAMT1,2\\
+                    gene_symbols[idx - 1] = gene_symbols[idx -
+                                                         1] + ',' + gene_symbol
+                    gene_symbols[idx] = ''
+
+                if len(gene_symbol) == 1:
+                    gene_symbols[idx] = ''
+
+            if is_there_symbol_to_be_replaced:
+                gene_symbols = list(filter(None, gene_symbols))
+
+                for accession in accessions:
+                    accession = accession.strip()
+                    if accession:
+                        get_pubmed_per_gene(accession, gene_symbols + [accession] + iricname + raprepname + rappredname,
+                                            annotated_abstracts, output_directory)
+
+                        print(f'Finished parsing entry for {accession}')
+
+    print(f'Finished post-processing handling gene symbols to be replaced')
 
 
 if __name__ == '__main__':
@@ -247,12 +251,27 @@ if __name__ == '__main__':
         'annotated_abstracts_file',
         help='file containing the annotated abstracts')
     parser.add_argument(
+        'symbol_replacement_file',
+        help='file containing the replacement for selected symbols'
+    )
+    parser.add_argument(
         'output_dir', help='output directory for the dictionary resulting from preprocessing the QTARO annotation file')
 
     args = parser.parse_args()
 
+    with open(args.symbol_replacement_file) as f:
+        symbols_to_be_replaced = {}
+        csv_reader_e = csv.reader(f, delimiter='\t')
+        for line in csv_reader_e:
+            symbol = line[0]
+            replacement = line[1].strip().split(',')
+            symbols_to_be_replaced[symbol] = replacement
+
     # get_pubmed_for_all_genes(args.gene_index_file,
     #                          args.annotated_abstracts_file, args.output_dir)
 
-    handle_english_symbols(args.gene_index_file,
-                           args.annotated_abstracts_file, args.output_dir)
+    # handle_english_symbols(args.gene_index_file,
+    #                        args.annotated_abstracts_file, args.output_dir)
+
+    # handle_symbol_replacement(args.gene_index_file,
+    #                           args.annotated_abstracts_file, args.output_dir)
