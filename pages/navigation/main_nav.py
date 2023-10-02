efba340bdd014a74edf3103090b3e35d4a2e0a6a
unknown <phoebs_ong@dlsu.edu.ph>
2023-10-02 20:04:46 +0800

    Change logo

--- pages/navigation/main_nav.py
+++ pages/navigation/main_nav.py
@@ -13,9 +13,9 @@ def navbar():
         id='top-navbar',
         brand=[dbc.Row(
             [
-                dbc.Col(html.Img(src='assets/logo.png', height='20px',
+                dbc.Col(html.Img(src='assets/rice_pilaf_logo.png', height='30px',
                         className='mx-auto'), className='d-flex align-items-center'),
-                dbc.Col(dbc.NavbarBrand('RicePilaf', className='ms-3')),
+                dbc.Col(dbc.NavbarBrand('RicePilaf', className='ms-1')),
             ],
             align="center",
             className="g-0",
