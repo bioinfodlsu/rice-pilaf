f11949e61c8d643a5f792792ccdd4bcbfb487090
memgonzales <gonzales.markedward@gmail.com>
2023-12-16 23:02:49 +0800

    Add user-facing elements for generating summary table

--- callbacks/summary/callbacks.py
+++ callbacks/summary/callbacks.py
@@ -1,4 +1,4 @@
-from dash import Input, Output, State, html
+from dash import Input, Output, State, html, ctx
 from dash.exceptions import PreventUpdate
 
 from ..lift_over import util as lift_over_util
@@ -9,12 +9,23 @@ def init_callback(app):
         Output('summary-genomic-intervals-input', 'children'),
         State('homepage-submitted-genomic-intervals', 'data'),
         Input('homepage-is-submitted', 'data'),
+        Input('summary-submit', 'n_clicks')
     )
-    def display_input(nb_intervals_str, homepage_is_submitted):
+    def display_input(nb_intervals_str, homepage_is_submitted, *_):
         if homepage_is_submitted:
             if nb_intervals_str and not lift_over_util.is_error(lift_over_util.get_genomic_intervals_from_input(nb_intervals_str)):
                 return [html.B('Your Input Intervals: '), html.Span(nb_intervals_str)]
             else:
                 return None
+
+        raise PreventUpdate
+
+    @app.callback(
+        Output('summary-converter-modal', 'is_open'),
+        Input('summary-converter-tooltip', 'n_clicks'),
+    )
+    def open_modals(summary_converter_tooltip_n_clicks):
+        if ctx.triggered_id == 'summary-converter-tooltip' and summary_converter_tooltip_n_clicks > 0:
+            return True
 
         raise PreventUpdate
--- pages/analysis/summary.py
+++ pages/analysis/summary.py
@@ -1,5 +1,33 @@
 from dash import html
 from callbacks.constants import Constants
+import dash_bootstrap_components as dbc
+
+
+# =====================
+# Miscellaneous Modals
+# =====================
+
+converter_modal = dbc.Modal([
+    dbc.ModalHeader(
+        dbc.ModalTitle('Rice ID Converter')
+    ),
+    dbc.ModalBody([
+        html.P(
+            'RicePilaf requires MSU accession IDs (i.e., IDs prefixed by "LOC_Os"). '),
+        html.P([
+            'To convert across different rice IDs, you may use this ',
+            html.A(
+                'tool', href='https://rapdb.dna.affrc.go.jp/converter/', target='_blank'
+            ),
+            ' from The Rice Annotation Project Database.'
+        ])
+    ])],
+    id='summary-converter-modal',
+    is_open=False,
+    size='lg',
+    scrollable=True
+)
+
 
 layout = html.Div(
     id={
@@ -36,5 +64,30 @@ layout = html.Div(
         ], className='analysis-intro p-3'),
 
         html.Br(),
+
+        html.Div([
+            dbc.Label(['Include additional genes from the pangenome lift-over or the text mining results',
+                       html.I(
+                           className='bi bi-info-circle', id='summary-converter-tooltip', n_clicks=0)]),
+            html.Br(),
+            dbc.Label(
+                'Enter their MSU accession IDs, separated by a semicolon (e.g., LOC_Os01g03680; LOC_Os01g03690; LOC_Os01g04110)',
+                className='small text-muted'),
+
+            converter_modal,
+            dbc.Alert(
+                id='summary-addl-genes-error',
+                color='danger',
+                style={'display': 'none'}
+            ),
+            dbc.Textarea(id='summary-addl-genes'),
+
+            html.Br(),
+
+            dbc.Button('Generate Summary',
+                       id='summary-submit',
+                       n_clicks=0,
+                       className='page-button'),
+        ], className='analysis-intro p-3'),
     ]
 )
