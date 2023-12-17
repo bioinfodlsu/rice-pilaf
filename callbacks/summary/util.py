fd5cba1a1a3aecc00d5ba287cf7135473d565858
memgonzales <gonzales.markedward@gmail.com>
2023-12-18 07:42:59 +0800

    Change homologs to orthologs

--- callbacks/summary/util.py
+++ callbacks/summary/util.py
@@ -11,21 +11,21 @@ def get_liftover_summary(genomic_interval):
     NB_IDX = 1
     NB_PREFIX = 'LOC_Os'
 
-    gene_to_homologs_map = defaultdict(set)
+    gene_to_orthologs_map = defaultdict(set)
     for row in all_genes:
         if row[NB_IDX].startswith(NB_PREFIX):
             # Subtract 2 (i.e., subtract OGI)
             for gene in row:
                 if gene != NULL_PLACEHOLDER:
-                    gene_to_homologs_map[row[NB_IDX]].add(gene)
+                    gene_to_orthologs_map[row[NB_IDX]].add(gene)
 
     gene_to_count = []
-    for gene, homologs in gene_to_homologs_map.items():
+    for gene, orthologs in gene_to_orthologs_map.items():
         # Subtract 2 to remove OGI and Nipponbare
-        gene_to_count.append([gene, len(homologs) - 2])
+        gene_to_count.append([gene, len(orthologs) - 2])
 
     gene_to_count_df = pd.DataFrame(
-        gene_to_count, columns=['Gene', '# Homologs'])
+        gene_to_count, columns=['Gene', '# Orthologs'])
     return gene_to_count_df
 
 
