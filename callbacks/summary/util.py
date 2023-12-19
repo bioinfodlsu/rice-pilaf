75e32b993e17904b886e04dd98b0ecb5b1f26507
memgonzales <gonzales.markedward@gmail.com>
2023-12-19 21:31:43 +0800

    Add summary table for QTL and PubMed

--- callbacks/summary/util.py
+++ callbacks/summary/util.py
@@ -4,15 +4,12 @@ from ..general_util import *
 from collections import defaultdict
 
 
-def get_liftover_summary(genomic_interval):
-    all_genes = get_all_genes(other_ref_genomes.keys(),
-                              genomic_interval).values.tolist()
-
+def get_liftover_summary(implicated_genes):
     NB_IDX = 1
     NB_PREFIX = 'LOC_Os'
 
     gene_to_orthologs_map = defaultdict(set)
-    for row in all_genes:
+    for row in implicated_genes:
         if row[NB_IDX].startswith(NB_PREFIX):
             # Subtract 2 (i.e., subtract OGI)
             for gene in row:
@@ -25,10 +22,50 @@ def get_liftover_summary(genomic_interval):
         gene_to_count.append([gene, len(orthologs) - 2])
 
     gene_to_count_df = pd.DataFrame(
-        gene_to_count, columns=['Gene', '# Orthologs'])
+        gene_to_count, columns=['Name', '# Orthologs'])
 
     return gene_to_count_df
 
 
+def get_num_qtl_pubs(qtl_str):
+    # Each QTL study has an associated DOI
+    return qtl_str.count('doi.org')
+
+
+def get_num_pubmed_pubs(pubmed_str):
+    # Each PubMed study has an associated PubMed link
+    return pubmed_str.count('pubmed')
+
+
+def get_qtl_summary(genomic_interval):
+    genes = get_genes_in_Nb(genomic_interval)[0]
+    genes['# QTL Analyses'] = genes.apply(
+        lambda x: get_num_qtl_pubs(x['QTL Analyses']), axis=1)
+
+    return genes[['Name', '# QTL Analyses']]
+
+
+def get_pubmed_summary(genomic_interval):
+    genes = get_genes_in_Nb(genomic_interval)[0]
+    genes['# PubMed Article IDs'] = genes.apply(
+        lambda x: get_num_pubmed_pubs(x['PubMed Article IDs']), axis=1)
+
+    return genes[['Name', '# PubMed Article IDs']]
+
+
 def make_summary_table(genomic_interval):
-    return get_liftover_summary(genomic_interval)
+    implicated_genes = get_all_genes(other_ref_genomes.keys(),
+                                     genomic_interval).values.tolist()
+
+    liftover_summary = get_liftover_summary(implicated_genes)
+    qtl_summary = get_qtl_summary(genomic_interval)
+    pubmed_summary = get_pubmed_summary(genomic_interval)
+
+    summary = liftover_summary.merge(
+        qtl_summary, on='Name', how='left', validate='one_to_one')
+    summary = summary.merge(pubmed_summary, on='Name',
+                            how='left', validate='one_to_one')
+
+    summary = summary.rename(columns={'Name': 'Gene'})
+
+    return summary
