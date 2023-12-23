967616e9e58ad6d44f021e5dde1394ae00e34c9f
memgonzales <gonzales.markedward@gmail.com>
2023-12-23 13:49:48 +0800

    Recover script for getting nodes from network

---
+++ prepare_data/workflow/scripts/network_util/get-nodes-from-network.py
@@ -0,0 +1,36 @@
+import os
+
+
+def get_nodes(network_file, output_dir):
+    all_nodes = set()
+
+    with open(network_file) as network:
+        for edge in network:
+            edge = edge.rstrip()
+            nodes = edge.split('\t')
+
+            all_nodes.add(nodes[0])
+            all_nodes.add(nodes[1])
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(f'{output_dir}/all-genes.txt', 'w') as f:
+        f.write('\n'.join(list(all_nodes)))
+        f.write('\n')
+
+    print(f"Wrote {len(all_nodes)} nodes to {output_dir}/all-genes.txt")
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'network_file', help='text file corresponding to the edge list')
+    parser.add_argument(
+        'output_dir', help='output directory for the list of nodes'
+    )
+
+    args = parser.parse_args()
+
+    get_nodes(args.network_file, args.output_dir)
