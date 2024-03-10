aed648ad119fe6fbe6f50d2172c5a9dd55432a93
memgonzales <gonzales.markedward@gmail.com>
2024-03-10 22:39:05 +0800

    Add command for getting latest release version to Dockerfile

--- callbacks/branch.py
+++ callbacks/branch.py
@@ -1,22 +1,21 @@
 from pathlib import Path
-import requests
 
 
 def get_active_branch_name():
     """
     Lifted from https://stackoverflow.com/questions/26134026/how-to-get-the-current-checked-out-git-branch-name-through-pygit2
     """
-    head_dir = Path(".") / ".git" / "HEAD"
-    with head_dir.open("r") as f:
+    head_dir = Path('.') / '.git' / 'HEAD'
+    with head_dir.open('r') as f:
         content = f.read().splitlines()
 
     for line in content:
-        if line[0:4] == "ref:":
-            return line.partition("refs/heads/")[2]
+        if line[0:4] == 'ref:':
+            return line.partition('refs/heads/')[2]
 
 
 def is_deployed_version():
-    return Path("./.deploy").exists()
+    return Path('./.deploy').exists()
 
 
 def is_in_demo_branch():
@@ -38,6 +37,10 @@ def show_if_not_in_demo_branch():
 
 
 def get_release_version():
-    latest_release = requests.get(
-        'https://github.com/bioinfodlsu/rice-pilaf/releases/latest')
-    return latest_release.url.split('/')[-1]
+    try:
+        version_file = Path('.version')
+        with version_file.open('r') as f:
+            for line in f:
+                return line.strip()
+    except:
+        return ''
