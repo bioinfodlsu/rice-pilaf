c520cdd251a478665c6e2a3c165a21d1b1dd7a36
memgonzales <gonzales.markedward@gmail.com>
2024-04-08 00:01:09 +0800

    Add logging parameters

--- generate_config.py
+++ generate_config.py
@@ -2,10 +2,12 @@ import requests
 import argparse
 
 
-def generate_config(debug, deployed, version=None):
+def generate_config(debug, deployed, log, prod_db, version=None):
     with open(".env", "w") as f:
         f.write(f"DEBUG={debug}\n")
         f.write(f"DEPLOYED={deployed}\n")
+        f.write(f"LOG={log}\n")
+        f.write(f"PROD_DB={prod_db}\n")
 
         if version:
             if version == "latest":
@@ -23,16 +25,27 @@ if __name__ == "__main__":
     parser.add_argument(
         "--debug", action="store_true", help="Enable Flask's debugging mode"
     )
+
     parser.add_argument(
         "--deployed",
         action="store_true",
         help="Use the deployed (instead of the local) version",
     )
+
+    parser.add_argument(
+        "--log", action="store_true", help="Enable logging for app usage analytics"
+    )
+
+    parser.add_argument(
+        "--prod-db", action="store_true", help="Use the production database"
+    )
+
     parser.add_argument(
         "--latest-version",
         action="store_true",
         help="Automatically tag the app with the latest version number. If this flag is set to True, the version manually specified using --version is overridden",
     )
+
     parser.add_argument(
         "-v",
         "--version",
@@ -46,4 +59,4 @@ if __name__ == "__main__":
     if args.latest_version:
         version = "latest"
 
-    generate_config(args.debug, args.deployed, version)
+    generate_config(args.debug, args.deployed, args.log, args.prod_db, version)
